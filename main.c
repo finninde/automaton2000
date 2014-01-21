ba1baea59d15125c906c00945c2bcd2169d99e3a
irxess <irxess@gmail.com>
2014-01-21 17:19:23 +0100

    used constants instead of numbers

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #define INPIN 7
 
 void setup() { 
-    pinMode(13, OUTPUT); // built in LED
-    pinMode(7, INPUT_PULLUP); // pullDown switch
+    pinMode(LEDPIN, OUTPUT); // built in LED
+    pinMode(INPIN, INPUT_PULLUP); // pullDown switch
     Serial.begin(9600);  // 9600 bps
     Serial.println("Hello world!");
 }
 
 void loop(){
-    bool buttonState = digitalRead(7);
-    digitalWrite(13, buttonState);
+    bool buttonState = digitalRead(INPIN);
+    digitalWrite(LEDPIN, buttonState);
 }
 
